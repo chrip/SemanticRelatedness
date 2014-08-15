88c7b8472a67da64ca8e2a9718c9580b45e02035
Christoph Schaefer <chrisschaefer@uni-koblenz.de>
2014-08-15 11:47:28 +0200

    Performance tuning, bug fix for commonScores == 0

--- src/main/java/semanticRelatedness/VectorSpaceCentroid.java
+++ src/main/java/semanticRelatedness/VectorSpaceCentroid.java
@@ -1,11 +1,11 @@
 package semanticRelatedness;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -30,18 +30,18 @@ public class VectorSpaceCentroid {
 	String field = "contents";
 	DefaultSimilarity similarity;
 	
-	HashMap<Integer, Double> centroid; // docId and tfidf 
-	double maxScore;
+	double[] centroid;
 	int totalDocs;
+	double scoreSumLog;
 	
-	public HashMap<Integer, Double> getCentroid() {
+	public double[] getCentroid() {
 		return centroid;
-	}
-	public double getMaxScore() {
-		return maxScore;
 	}
 	public double numDocs() {
 		return totalDocs;
+	}
+	public double getScoreSumLog() {
+		return scoreSumLog;
 	}
 
 	VectorSpaceCentroid(String luceneIndexPath, String text) throws IOException, ParseException {
@@ -64,18 +64,22 @@ public class VectorSpaceCentroid {
 		
 		buildCentroid(text);
 	}
+	
+	VectorSpaceCentroid(double[] centroid){
+		totalDocs = centroid.length;
+		this.centroid = centroid;
+	}
 
 	public void buildCentroid(String text) throws ParseException, IOException {
 		HashMap<String, Integer> parsedTokensCount  = new HashMap<String, Integer>();
-		centroid = new HashMap<Integer, Double>();
-		maxScore = 0;
+		
+		double maxScore = 0;
 		
 		
 		TokenStream ts = parser.getAnalyzer().tokenStream(field, new StringReader(text));
 		CharTermAttribute termAttr = ts.addAttribute(CharTermAttribute.class);
         ts.reset();
 
-        int numTerms = 0;
         while (ts.incrementToken()) {
             String token = termAttr.toString();
             if(parsedTokensCount.containsKey(token)){
@@ -84,13 +88,12 @@ public class VectorSpaceCentroid {
 			else {
 				parsedTokensCount.put(token, 1);
 			}
-			numTerms++;
         }
         ts.end();
         ts.close();
 		
 		totalDocs = reader.numDocs();
-		double tfidfSum = 0;
+		HashMap<Integer, Double> centroidMap = new HashMap<Integer, Double>();
 		for (String parsedToken : parsedTokensCount.keySet()) {
 			int termFreq = parsedTokensCount.get(parsedToken);
 			Term term = new Term(field, parsedToken);
@@ -102,49 +105,59 @@ public class VectorSpaceCentroid {
 			if(results.totalHits < 1) {
 				continue;
 			}
+			double score = 0;
 			for(ScoreDoc doc : results.scoreDocs){
-				if(centroid.containsKey(doc.doc)){
-					centroid.put(doc.doc, centroid.get(doc.doc)+ (tfidf * doc.score));
+				if(centroidMap.containsKey(doc.doc)){
+					score = centroidMap.get(doc.doc)+ (tfidf * doc.score);
+					centroidMap.put(doc.doc, score);
 				}
 				else {
-					centroid.put(doc.doc, tfidf * doc.score);
+					score = tfidf * doc.score;
+					centroidMap.put(doc.doc, score);
 				}
 			}
-			tfidfSum += tfidf;
-		}
-		for (Map.Entry<Integer, Double> entry : centroid.entrySet()) {
-			double score = entry.getValue()/tfidfSum;
-			entry.setValue(score);
 			maxScore = Math.max(score, maxScore);
+		}
+		double sum = 0.0; 
+		for (Map.Entry<Integer, Double> entry : centroidMap.entrySet()) {
+			double score = entry.getValue()/maxScore;
+			sum += score;
+			entry.setValue(score);				
 	    }
+		scoreSumLog = Math.log(sum);
+		
+		centroid = new double[totalDocs];
+		for (int i = 0; i < totalDocs; i++) {
+		   if(centroidMap.containsKey(i)){
+			   centroid[i] = centroidMap.get(i);
+		   }
+		   else {
+			   centroid[i] = 0;
+		   }
+		}
 	}
 
 	private static double sumCommonScores(VectorSpaceCentroid centroid0, VectorSpaceCentroid centroid1){
 		double sum = 0;
-		double maxScore0 = centroid0.getMaxScore();
-		double maxScore1 = centroid1.getMaxScore();
-
-		for (final Map.Entry<Integer, Double> entry : centroid0.getCentroid().entrySet()) {
-			int doc0 = entry.getKey();
-
-			if (centroid1.getCentroid().containsKey(doc0)) {
-				double score0 = entry.getValue();
-				double score1 = centroid1.getCentroid().get(doc0);
-				double sim1AND2 = (score0/maxScore0)  * (score1/maxScore1);
-				sum += sim1AND2;				 
-			}			
+		double[] c0 = centroid0.getCentroid();
+		double[] c1 = centroid1.getCentroid();
+		for (int i = 0; i < c0.length && i < c1.length; i++) {
+			sum += c1[i] * c0[i];
 		}
 		return sum;
 	}
 	
+	
 	public static double normalizedRelevanceDistance(VectorSpaceCentroid centroid0, VectorSpaceCentroid centroid1){
 		double log0, log1 , logCommon, maxlog, minlog;
 
-		log0 = Math.log(sumScores(centroid0));
-		log1 = Math.log(sumScores(centroid1));
+		log0 = centroid0.getScoreSumLog();
+		log1 = centroid1.getScoreSumLog();
+
         double commonScore = sumCommonScores(centroid0, centroid1);
+        
 		if(commonScore == 0) {
-			return 0.5;
+			return 0.0;
 		}
 		logCommon = Math.log(commonScore);
 		maxlog = Math.max(log0, log1);
@@ -153,13 +166,24 @@ public class VectorSpaceCentroid {
 		return  Math.exp(-2*  (maxlog - logCommon) / (Math.log(centroid0.numDocs()) - minlog));
 	}
 	
-	private static double sumScores(VectorSpaceCentroid centroid) {
-		double sum = 0.0; 
-		double maxScore = centroid.getMaxScore();
+	public int json(BufferedWriter bw) throws IOException{
+		bw.write("{\"totalDocs\":" + totalDocs);
+		bw.write(",\"scores\":[");
 
-		for (double score : centroid.getCentroid().values()) {
-			sum += (score/maxScore);
+		for(int j = 0; j < totalDocs; j++){
+			if(j != 0){
+				bw.write(",");
+			}
+			if(centroid[j] == 0.0){
+				bw.write("0");
+			}
+			else {
+				bw.write(String.format("%.6f",centroid[j]));
+			}
+
 		}
-		return sum;
+		bw.write("]}");
+		return totalDocs;
 	}
+	
 }
